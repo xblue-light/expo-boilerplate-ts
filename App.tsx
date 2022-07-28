b5e1193eff0db7e1df4a927726a82f7d83b5feab
Paul Bowyer <paul.bowyer@hypernetlabs.io>
2022-07-28 21:05:02 +0300

    chore: add flex container properties

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
 import * as React from "react";
 import { ThemeProvider } from "@rneui/themed";
 import { alphaTheme } from "./app/styles/theme";
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
     <ThemeProvider theme={alphaTheme}>
-      <SafeAreaView
-        style={{
-          backgroundColor: "#cece",
-          flexDirection: "row",
-          flex: 1, // Take up available total space so then the children automatically will take up the full height (i.e text components)
-        }}
-      >
+      <SafeAreaView style={styles.flexContainer}>
         <Text style={styles.textEleOne}>ELE 1</Text>
         <Text style={styles.textEleTwo}>ELE 2</Text>
         <Text style={styles.textEleThr}>ELE 3</Text>
@@ -24,33 +18,31 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "#ccc",
+  flexContainer: {
+    flex: 1, // Take up available total space so then the children automatically will take up the full height (i.e text components).
+    flexDirection: "row", // default is column; column-reverse; row; row-reservse;
+    backgroundColor: "forestgreen",
+    justifyContent: "space-around", // Defines the space b/e children elements.
+    alignItems: "flex-end", // Defines how the inner children elements will be aligned within the flex container.
   },
   textEleOne: {
     backgroundColor: "rebeccapurple",
-    padding: 10,
+    padding: 16,
     color: "white",
-    textAlign: "left",
   },
   textEleTwo: {
     backgroundColor: "coral",
-    padding: 10,
+    padding: 16,
     color: "white",
-    textAlign: "left",
   },
   textEleThr: {
     backgroundColor: "lightblue",
-    padding: 10,
+    padding: 16,
     color: "white",
-    textAlign: "left",
   },
   textEleFou: {
     backgroundColor: "pink",
-    padding: 10,
+    padding: 16,
     color: "white",
-    textAlign: "left",
   },
 });
