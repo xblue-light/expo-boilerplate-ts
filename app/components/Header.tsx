4defb3e329b94ab38d971c7d905ec707dc2e11af
Paul Bowyer <paul.bowyer@hypernetlabs.io>
2022-08-03 19:23:08 +0300

    chore: remove .dotenv fake access keys

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,36 +3,34 @@ import { Header, Icon } from "@rneui/base";
 import { View, TouchableOpacity } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export const HeaderAlpha = (): JSX.Element => {
-  return (
-    <Header
-      backgroundImageStyle={{}}
-      barStyle="default"
-      centerComponent={{
-        text: "Yellow Notes",
-        style: {
-          color: "#333",
-          paddingTop: 16,
-          fontSize: 16,
-          fontWeight: "bold",
-        },
-      }}
-      centerContainerStyle={{}}
-      containerStyle={globalStyles.headerContainer}
-      leftComponent={
-        <View style={globalStyles.headerLeftComponent}>
-          <TouchableOpacity
-            onPress={() => console.log("Hello you clicked menu icon 2")}
-          >
-            <Icon name="menu" color="white" size={35} />
-          </TouchableOpacity>
-        </View>
-      }
-      linearGradientProps={{}}
-      placement="center"
-      rightComponent={{ icon: "home", color: "#ffffff", size: 35 }}
-      rightContainerStyle={{ paddingTop: 8 }}
-      statusBarProps={{}}
-    />
-  );
-};
+export const HeaderAlpha = (): JSX.Element => (
+  <Header
+    backgroundImageStyle={{}}
+    barStyle="default"
+    centerComponent={{
+      text: "Yellow Notes",
+      style: {
+        color: "#333",
+        paddingTop: 16,
+        fontSize: 16,
+        fontWeight: "bold",
+      },
+    }}
+    centerContainerStyle={{}}
+    containerStyle={globalStyles.headerContainer}
+    leftComponent={
+      <View style={globalStyles.headerLeftComponent}>
+        <TouchableOpacity
+          onPress={() => console.log("Hello you clicked menu icon 2")}
+        >
+          <Icon name="menu" color="white" size={35} />
+        </TouchableOpacity>
+      </View>
+    }
+    linearGradientProps={{}}
+    placement="center"
+    rightComponent={{ icon: "home", color: "#ffffff", size: 35 }}
+    rightContainerStyle={{ paddingTop: 8 }}
+    statusBarProps={{}}
+  />
+);
