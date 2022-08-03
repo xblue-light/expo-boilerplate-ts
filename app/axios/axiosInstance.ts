2726daa551e92d5c6cbea2b5abeff4443acfe616
Paul Bowyer <paul.bowyer@hypernetlabs.io>
2022-08-03 21:20:38 +0300

    chore: refactor axios post method

--- app/axios/axiosInstance.ts
+++ app/axios/axiosInstance.ts
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 const baseUrl = "http://10.0.2.2:3000";
 
-// Invoking get method to perform a GET request
+// Invoking get method to perform a POST request
 export const loginWithMockUserCreds = async () => {
   try {
     const url = `${baseUrl}/login`;
